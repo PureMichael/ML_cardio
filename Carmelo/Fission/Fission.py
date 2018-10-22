b794ab88a38c590d85250d9adbcdecda85d59b25
Carmelo S. Gonzales <GonzalesC@DEV.NET>
2018-10-22 12:01:47 -0700

    most recent code

--- Carmelo/Fission/Fission.py
+++ Carmelo/Fission/Fission.py
@@ -3,48 +3,70 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-# import keras
-# from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, BatchNormalization, ReLU
-# from keras.models import Model
+import os
+import keras
+from keras.utils import to_categorical
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, BatchNormalization, ReLU, Flatten, Softmax
+from keras.models import Model, Sequential
+
+# images = mpimg.imread('E:/Fission/Type1_Trace/17-32/121025 72011-98_X00945_Y00780_ID00986.png')
+# plt.imshow(images, cmap='gray')
+# plt.show()
+# images = Image.open('E:/Fission/Type1_Trace/17-32/121025 72011-98_X00945_Y00780_ID00986.png')
+# images = Image.open('C:/Users/gonzalesc/Desktop/72011-94_X00086_Y01803_ID00795.jpg')
+# print(images)
+# images.show()
 
 
-image = mpimg.imread('E:/Fission/Type1_Trace/17-32/121025 72011-98_X00945_Y00780_ID00986.png')
-# image = Image.open('E:/Fission/Type1_Trace/17-32/121025 72011-98_X00945_Y00780_ID00986.png')
-# image = Image.open('C:/Users/gonzalesc/Desktop/72011-94_X00086_Y01803_ID00795.jpg')
-# image.show()
-
-# print(type(image))
-plt.imshow(image, cmap='gray')
-plt.show(block=False)
-image[:,:,1]=image
-print(image.shape())
-
-image =  image.resize((100,100), PIL.Image.BICUBIC)
+dir = 'E:/Images_Processed/'
+numFiles = (len(os.listdir(dir)))
+images = np.empty((numFiles,176,176))
+labels = np.ones(numFiles)
+for i, filename in enumerate(os.listdir(dir)):
+    if np.mod(i,100)==0:
+        print(np.round(100*i/numFiles,1))
+    images[i,:,:]=mpimg.imread(dir+filename)
+    if filename[0]=='n':
+        labels[i]=0
 
 
-# image = np.reshape(image, (7*177,7*177))
-plt.imshow(image, cmap='gray')
-plt.show()
+cat_no = np.nonzero(labels>0)
 
 
 
-# input_img = Input(shape=(176, 176, 1))
-# x = Conv2D(8,(3, 3,), activation='relu', padding='same')(input_img)
-# x = BatchNormalization(axis=1)(x)
-# # x = ReLU()(x)
-# x = MaxPooling2D((2, 2), strides=3, padding='same')(x)
-# x = Conv2D(32,(3, 3,), activation='relu', padding='same')(x)
-# x = BatchNormalization(axis=1)(x)
-# # x = ReLU()(x)
-# x = Dense(2, input_shape=(x.shape[0],x.shape[1]), activation='softmax')
-# classifier = Model(input_img, x)
-# sgd = keras.optimizers.SGD(lr=0.01)
-# classifier.compile(loss='crossentropy',
-#                    optimizer='sgd')
-#
-# classifier.fit(images, labels,
-#                epochs=10,
-#                shuffle=True,
-#                validation_data=(test_images, test_labels))
+trainNum = np.round(0.75*len(cat_no[0][:]))
+
+
+
+
+labels = to_categorical(labels)
+print(labels)
+images = images.astype('float32')/255
+print(len(images))
+images = np.reshape(images, (len(labels), 176, 176, 1))
+
+
+model = Sequential()
+model.add(Conv2D(8,(3, 3,), activation='relu', input_shape=(176, 176, 1), padding='same'))
+model.add(BatchNormalization())
+model.add(ReLU())
+model.add(MaxPooling2D((2, 2), strides=3, padding='same'))
+model.add(Conv2D(32,(3, 3,), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(ReLU())
+model.add(MaxPooling2D((3, 3), strides=3, padding='same'))
+model.add(Conv2D(32,(3, 3,), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(ReLU())
+model.add(Flatten())
+model.add(Dense(2, activation='softmax'))
+model.add(Softmax())
+sgd = keras.optimizers.SGD(lr=0.01)
+model.compile(loss='categorical_crossentropy',
+                   optimizer='sgd')
+
+model.fit(images, labels,
+               epochs=10,
+               shuffle=True)
 
 
