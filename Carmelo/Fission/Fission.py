ccd15ccf43fca7e86834ab4f100890bf0f186619
Carmelo S. Gonzales <GonzalesC@DEV.NET>
2018-10-31 12:01:18 -0700

    Cant get net to work. Always 50 percent classification

--- Carmelo/Fission/Fission.py
+++ Carmelo/Fission/Fission.py
@@ -1,6 +1,7 @@
 import keras
+import tensorflow as tf
 from keras.utils import to_categorical
-from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, BatchNormalization, ReLU, Flatten, Softmax
+from keras.layers import Input, Activation, Dropout, Dense, Conv2D, MaxPooling2D, UpSampling2D, BatchNormalization, ReLU, Flatten, Softmax
 from keras.models import Model, Sequential
 
 import numpy as np
@@ -9,12 +10,18 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import os
 
+nn=6
+aa=4
+random.seed(nn+aa)
+tf.set_random_seed(nn+aa)
+np.random.seed(nn+aa)
+
 dir = 'E:/Images_Processed/'
 numFiles = (len(os.listdir(dir)))
 images = np.empty((numFiles,176,176))
 labels = np.ones(numFiles)
 for i, filename in enumerate(os.listdir(dir)):
-    if np.mod(i,100)==0:
+    if np.mod(i,100)==0 or i==numFiles-1:
         print(np.round(100*i/numFiles,1))
     images[i,:,:]=mpimg.imread(dir+filename)
     if filename[0]=='n':
@@ -53,6 +60,14 @@ test_imgs = np.append(yes_test_img, no_test_img, 0)
 test_labels = np.append(yes_test_labels, no_test_labels)
 ###
 ###
+randomize_train = random.sample(range(len(train_imgs)), len(train_imgs))
+randomize_test = random.sample(range(len(test_imgs)), len(test_imgs))
+train_imgs = train_imgs[randomize_train, :, :]
+train_labels = train_labels[randomize_train]
+test_imgs = test_imgs[randomize_test, :, :]
+test_labels = test_labels[randomize_test]
+###
+###
 train_imgs = train_imgs.astype('float32')/255
 train_imgs = np.reshape(train_imgs, (len(train_labels), 176, 176, 1))
 train_labels = to_categorical(train_labels)
@@ -60,12 +75,39 @@ test_imgs = test_imgs.astype('float32')/255
 test_imgs = np.reshape(test_imgs, (len(test_labels), 176, 176, 1))
 test_labels = to_categorical(test_labels)
 ###
+###
+n=10
+random_train_image_idx = random.sample(range(len(train_imgs)),n)
+random_test_image_idx = random.sample(range(len(test_imgs)),n)
+plt.figure(figsize=(10,4))
+plt.suptitle('10 Random Train and Test Images w/ Label')
+for i in range(n):
+    ax = plt.subplot(2, n, i+1)
+    plt.imshow(train_imgs[random_train_image_idx[i]].reshape(176,176))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    ax.set_title(str(train_labels[random_train_image_idx[i]][1]))
+    # ax.set_title(str(train_labels[random_train_image_idx[i]]))
+    ax = plt.subplot(2, n, i+n+1)
+    plt.imshow(test_imgs[random_test_image_idx[i]].reshape(176,176))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    ax.set_title(str(test_labels[random_test_image_idx[i]][1]))
+    # ax.set_title(str(test_labels[random_test_image_idx[i]]))
+
+plt.show(block=False)
+plt.pause(5)
+plt.close()
+###
+###
 print('Creating Model')
 model = Sequential()
-model.add(Conv2D(8,(3, 3,), activation='relu', input_shape=(176, 176, 1), padding='same'))
+model.add(Conv2D(8,(3, 3,), input_shape=(176, 176, 1), padding='same'))
 model.add(BatchNormalization())
 model.add(ReLU())
-model.add(MaxPooling2D((2, 2), strides=3, padding='same'))
+model.add(MaxPooling2D((2, 2), strides=2, padding='same'))
 model.add(Conv2D(32,(3, 3,), activation='relu', padding='same'))
 model.add(BatchNormalization())
 model.add(ReLU())
@@ -74,21 +116,37 @@ model.add(Conv2D(32,(3, 3,), activation='relu', padding='same'))
 model.add(BatchNormalization())
 model.add(ReLU())
 model.add(Flatten())
-model.add(Dense(2, activation='softmax'))
-model.add(Softmax())
+model.add(Dense(2, activation='sigmoid'))
 print('Compiling Model')
 sgd = keras.optimizers.SGD(lr=0.01)
-model.compile(loss='categorical_crossentropy',
-                   optimizer='sgd')
+model.compile(loss='binary_crossentropy',
+                   optimizer=sgd,
+              metrics=['accuracy'])
+model.summary()
 print('Fitting Model')
 model.fit(train_imgs, train_labels,
-               epochs=10,
-               batch_size=128,
+               epochs=1,
+               batch_size=234,
                shuffle=True,
                validation_data=(test_imgs, test_labels))
-print('Scoring Model')
-scoresTrain = model.evaluate(train_imgs, train_labels)
-print("\n%sL %.2f%%" % (model.metrics_names[1], scoresTrain[1]*100))
-scoresTest = model.evaluate(test_imgs, test_labels)
-print("\n%sL %.2f%%" % (model.metrics_names[1], scoresTest[1]*100))
+# print('Scoring Model')
+# scoresTrain = model.evaluate(train_imgs, train_labels)
+# print("\n%sL %.2f%%" % (model.metrics_names[1], scoresTrain[1]*100))
+# scoresTest = model.evaluate(test_imgs, test_labels)
+# print("\n%sL %.2f%%" % (model.metrics_names[1], scoresTest[1]*100))
+predicted_imgs = model.predict_classes(test_imgs,verbose=True)
+print(np.column_stack((predicted_imgs, test_labels[:][1])))
+predicted_test_image_idx = random.sample(range(len(predicted_imgs)),10)
+n=10
+plt.figure(figsize=(10,4))
+plt.suptitle('Test Image w/ Predicted Label')
+for i in range(n):
+    ax = plt.subplot(1, n, i+1)
+    plt.imshow(test_imgs[predicted_test_image_idx[i]].reshape(176,176))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    ax.set_title(str(predicted_imgs[i]))
+
+plt.show()
 
