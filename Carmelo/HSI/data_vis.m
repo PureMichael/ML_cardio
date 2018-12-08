e877e63be5ed626d02567ab4e9d9d9979b748211
melo-gonzo <carmelojsg@gmail.com>
2018-12-07 16:39:20 -0800

    small changes

--- Carmelo/HSI/data_vis.m
+++ Carmelo/HSI/data_vis.m
@@ -1,19 +1,19 @@
 close all
 clear all
 clc
-norm_them=1;
+norm_them=0;
 wavelength=csvread('GT_Test.csv',0,2,[0 2 0 171]);
 M=csvread('GT_Test.csv',1,1);
 M(:,1)=M(:,1)+1;
 cats=unique(M(:,1));
 
 if norm_them ==1
-    M(:,2:end)=0.1+ M(:,2:end)-min(M(:,2:end),[],2);
+%     M(:,2:end)=0.1+ M(:,2:end)-min(M(:,2:end),[],2);
     M(:,2:end)=M(:,2:end)./max(M(:,2:end),[],2);
 end 
 
 
-%%
+%% Plot the average of all categories 
 figure();hold on
 for ii=1:length(cats)
     d(cats(ii)).data=M(M(:,1)==cats(ii),2:end);
@@ -23,11 +23,11 @@ end
 legend(legend_entries)
 axis tight
 box on
-%%
+%% Plot 50 random spectra from each category on its own figure
 for ii=1:length(cats)
     ii    
     figure();
-    plot_these = randi(length(d(ii).data(:,1)),50);
+    plot_these = randi(length(d(ii).data(:,1)),50,1);
     line = plot(0,nan(length(plot_these),1));
     xd=(1:length(d(ii).data(1,:)))-1;
     for jj=1:length(plot_these)
@@ -42,7 +42,7 @@ end
 
 %%
 figure()
-pt = randi(length(M(:,1)),5);
+pt = randi(length(M(:,1)),5,1);
 hold on
 for ii=1:length(pt)
     plot(M(pt(ii),2:end))
